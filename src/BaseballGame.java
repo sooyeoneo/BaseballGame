60935271c086b90990a405a108413b4949c180ff
sooyeon_heo <suzi97@naver.com>
2024-10-23 18:38:06 +0900

    숫자 야구 게임 v.1.1 -> 랜덤 3자리 수 생성

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,5 +1,39 @@
-public class BaseballGame {
-    public BaseballGame() {
+import java.util.*;
 
+public class BaseballGame {
+
+    public BaseballGame() {
+        Random random = new Random();
+        Set<Integer> NumberGenerator = new HashSet<>();
+
+        while (NumberGenerator.size() < 3) {
+            int number = random.nextInt(9) + 1; // 1부터 9 사이의 숫자
+            NumberGenerator.add(number);
+        }
+        // 객체 생성시 랜덤 3자리 수의 정답을 만든다. ..? ㅠㅠ
+        // System.out.println("생성된 3자리 수: " + NumberGenerator);
     }
+
+//    public int play() {
+//        while (true) {
+//            // 1. 사용자에게 입력값을 받는다.
+//            // 2. 올바른 입력값을 받았는지 검증
+//            // 3. 게임 진행횟수 증가
+//            // 4. 스트라이크 개수 계산
+//            // 5. 정답여부 확인, 만약 정답이면 break를 이용해 반복문 종료
+//            // 6. 볼 개수 계산
+//            // 7. 힌트 출력
+//        }
+//        // 게임 진행횟수 반환
+//    }
+//    protected boolean validateInput(String input) {
+//
+//    }
+//    private int countStrike(String input) {
+//
+//    }
+//
+//    private int countBall(String input) {
+//        return 0;
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // BaseballGame 객체 생성 & 게임 시작
+        // BaseballGame 객체 생성 시 랜덤한 3자리 수가 출력된다.
+        BaseballGame baseballGame = new BaseballGame();
+
     }
 }
