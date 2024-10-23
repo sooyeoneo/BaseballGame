6c6d0892d504c55ac361fbcb7b75d70411e7cf81
sooyeon_heo <suzi97@naver.com>
2024-10-23 16:49:31 +0900

    숫자 야구 게임 v.1 -> BaseballGame 객체 생성

---
+++ src/BaseballGame.java
@@ -0,0 +1,5 @@
+public class BaseballGame {
+    public BaseballGame() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        // BaseballGame 객체 생성 & 게임 시작
+    }
+}
