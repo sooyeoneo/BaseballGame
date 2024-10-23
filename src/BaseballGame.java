71fa4ba75cc2ee6014de9e03fd1d361a4ef334ac
sooyeon_heo <suzi97@naver.com>
2024-10-23 21:46:22 +0900

    숫자 야구 게임 v.1.2 -> 사용자에게 3자리수 입력값 받기

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,39 +1,30 @@
 import java.util.*;
 
 public class BaseballGame {
+    private final List<Integer> answerNumber; // 랜덤 3자리 수를 저장할 Set
 
     public BaseballGame() {
         Random random = new Random();
-        Set<Integer> NumberGenerator = new HashSet<>();
+        Set<Integer> baseballNumber = new LinkedHashSet<>(); // 삽입한 순서를 유지
 
-        while (NumberGenerator.size() < 3) {
+        while (baseballNumber.size() < 3) {
             int number = random.nextInt(9) + 1; // 1부터 9 사이의 숫자
-            NumberGenerator.add(number);
+            baseballNumber.add(number);
         }
-        // 객체 생성시 랜덤 3자리 수의 정답을 만든다. ..? ㅠㅠ
+        answerNumber = new ArrayList<>(baseballNumber); // set은 순서보장이 안됨. list로 index 사용
+        // 객체 생성시 랜덤 3자리 수의 정답을 만든다.
         // System.out.println("생성된 3자리 수: " + NumberGenerator);
     }
 
-//    public int play() {
-//        while (true) {
-//            // 1. 사용자에게 입력값을 받는다.
-//            // 2. 올바른 입력값을 받았는지 검증
-//            // 3. 게임 진행횟수 증가
-//            // 4. 스트라이크 개수 계산
-//            // 5. 정답여부 확인, 만약 정답이면 break를 이용해 반복문 종료
-//            // 6. 볼 개수 계산
-//            // 7. 힌트 출력
-//        }
-//        // 게임 진행횟수 반환
-//    }
-//    protected boolean validateInput(String input) {
-//
-//    }
-//    private int countStrike(String input) {
-//
-//    }
-//
-//    private int countBall(String input) {
-//        return 0;
-//    }
+    public int play() {
+
+        Scanner sc = new Scanner(System.in); // 사용자에게 입력값을 받는다.
+        while (true) {
+            System.out.println("숫자를 입력하세요 (중복되지 않은 3자리 수) : ");
+            String userInput = sc.next(); // 문자열로 입력 받는다.
+
+
+        }
+        // 게임 진행횟수 반환
+    }
 }
