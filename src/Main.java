72e8ad367718688aa744479ab4549c958dbcb205
sooyeon_heo <suzi97@naver.com>
2024-10-25 12:20:01 +0900

    숫자 야구 게임 v.3.1 -> 컬렉션 Map(Key, Value)으로 게임 기록(n번째 게임 순번 횟수, 정답을 맞힐 때 까지 시도 횟수)하기

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int gameCount = 0; // n번째 게임 순번 횟수 초기화
+        Map<Integer, Integer> store = new HashMap<>(); // 게임 전체 횟수 기록 -> Map
 
         while (true) {
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
@@ -12,9 +14,6 @@ public class Main {
 
             String selection = sc.next(); // 사용자로부터 1, 2, 3 입력 받기
             System.out.println(); // 메뉴와 게임 실행 간의 간격
-
-            int gameCount = 0; // 게임 횟수 초기화
-            Map<Integer, Integer> store = new HashMap<>(); // 게임 전체 횟수 기록 -> Map
 
             switch (selection) {
                 case "1": // 1을 입력하면 게임 실행 및 재시작 여부 확인
@@ -29,16 +28,19 @@ public class Main {
 
                         if (!retry.equalsIgnoreCase("네")) {
                             System.out.println("게임을 종료합니다.");
-                            for (Map.Entry<Integer, Integer> entry : store.entrySet()) {
-                                System.out.println(entry.getKey() + "번째 게임 : 시도 횟수 - " + entry.getValue());
-                            }
                             break; // 사용자가 "네"가 아닌 값을 입력 시 반복문 종료
                         }
                     }
                     break;
 
-                case "2": // 2를 입력하면 "게임 기록 보기" 기능 구현 예정
-                    System.out.println("게임 기록 보기 기능 보여줄 예정.");
+                case "2": // 2를 입력하면 "게임 기록 보기" 기능. n번째 게임 순번 횟수와 정답을 맞힐 때까지의 게임 시도 횟수
+                    if (store.isEmpty()) {
+                        System.out.println("아직 기록된 게임이 없습니다."); // 게임을 하기 전에 2번을 선택했을 경우
+                    } else {
+                        for (Map.Entry<Integer, Integer> entry : store.entrySet()) {
+                            System.out.println(entry.getKey() + "번째 게임 : 시도 횟수 - " + entry.getValue());
+                        } // getKey로 gameCount, getValue로 attmepts 값을 가져오기
+                    }
                     break;
 
                 case "3": // 3을 입력하면 게임 종료
